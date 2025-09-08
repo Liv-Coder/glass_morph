d7d28b1bc12f439c14954a22f19392f9487773c0
Naawaal <Naawaal@users.noreply.github.com>
2025-09-08 05:29:26 +0000

    deploy: 38d12aaa9d34f12fe243e354287d18766b743787

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1808032260"
+    serviceWorkerVersion: "3344916157"
   }
 });
